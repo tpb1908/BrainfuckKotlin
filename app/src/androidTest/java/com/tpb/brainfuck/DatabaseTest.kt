b956676ffaca30b043dac32cac0f32ef11f1b559
tpb1908 <theo1908phone@gmail.com>
2017-07-06 13:19:36 +0100

    Add tests for database.

--- app/src/androidTest/java/com/tpb/brainfuck/DatabaseTest.kt
+++ app/src/androidTest/java/com/tpb/brainfuck/DatabaseTest.kt
@@ -4,18 +4,27 @@ import android.support.test.InstrumentationRegistry
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.tpb.brainfuck.db.PointerOverflowBehaviour
+import com.tpb.brainfuck.db.Program
 import com.tpb.brainfuck.home.MainActivity
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
+import io.reactivex.rxkotlin.subscribeBy
+import org.junit.Assert.*
+import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+import kotlin.concurrent.thread
+
 /**
  * Created by theo on 05/07/17.
  */
 @RunWith(AndroidJUnit4::class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @LargeTest
 class DatabaseTest {
+
+
 
     val context = InstrumentationRegistry.getTargetContext()
     val dao = Application.db.programDao()
@@ -25,50 +34,89 @@ class DatabaseTest {
     val activityRule : ActivityTestRule<MainActivity> = ActivityTestRule<MainActivity>(MainActivity::class.java)
 
 
-    @Test fun useAppContext() {
+    @Test fun stage1_useAppContext() {
         System.out.println("Use app context is running")
         info("Use app context is running")
         assertEquals("com.tpb.brainfuck", context.packageName)
     }
 
-    @Test fun testDatabaseCreated() {
+    @Test fun stage2_testDatabaseCreated() {
         assertNotNull(Application.db)
     }
 
-    @Test fun testDefaultValuesContained() {
-        dao.getAllPrograms().subscribe { list ->
-//            var foundMatches = true
-//            Application.defaults.forEach {
-//                var foundMatch = false
-//                list.forEach { listItem ->
-//                    if (listItem.name == it.name) foundMatch = true
-//                }
-//                foundMatches = foundMatch
-//            }
-//            assertEquals(true, foundMatches)
-            Application.defaults.forEach { info("Default name: ${it.name}") }
-            list.forEach { info("List name: ${it.name}") }
-        }
-    }
-
-    @Test fun testRestorationOfDefaults() {
-        var section = 0
-        dao.getAllPrograms().subscribe {
-            if (section == 0) {
-                assertEquals(Application.defaults.size, it.size)
-                section++
-                dao.deleteAllPrograms()
-            } else if (section == 1) {
-                assertEquals(0, it.size)
-                section++
-                Application.restoreDefaultValues()
-            } else {
-                assertEquals(Application.defaults.size, it.size)
+    @Test fun stage3_testDefaultValuesContained() {
+        dao.getAllPrograms().take(1).subscribe { list ->
+            var foundMatches = true
+            Application.defaults.forEach {
+                var foundMatch = false
+                list.forEach { listItem ->
+                    if (listItem.name == it.name) {
+                        foundMatch = true
+                    }
+                }
+                foundMatches = foundMatch
             }
+            assertEquals(true, foundMatches)
 
         }
-
     }
 
+    @Test fun stage4_resetDatabase() {
+        thread {
+            dao.deleteAllPrograms()
+            dao.getAllPrograms().take(1).subscribe {
+                assertEquals(0, it.size)
+
+            }
+        }
+    }
+
+    @Test fun stage5_testRestorationOfDefaults() {
+        thread {
+            var count = 0
+            Application.restoreDefaultValues()
+            dao.getAllPrograms().take(Application.defaults.size.toLong()).subscribeBy(
+                    onNext = {count++},
+                    onComplete = { assertEquals(Application.defaults.size, count) }
+            )
+        }
+    }
+
+    @Test fun stage6_testInsertAndRetrieve() {
+        val p = Program(name = "Test")
+        thread {
+            p.uid = dao.insert(p)
+            assertEquals(p, dao.getProgram(p.uid))
+        }
+    }
+
+    @Test fun stage7_testFindByNameReturnsExistingProgram() {
+        val p = Program(name = "Program to find", source = "Some source")
+        thread {
+            p.uid = dao.insert(p)
+            assertEquals(p, dao.getProgramIfExists(p.name, p.source))
+        }
+    }
+
+    @Test fun stage8_testFindByNameDoesNotReturnNonExistentProgram() {
+        thread {
+            assertNull(dao.getProgramIfExists("nothing to find", "++>>"))
+        }
+    }
+
+    @Test fun state9_testUpdateOfProgram() {
+        val p = Program(name = "Initial name", source = "Some source", memoryCapacity= 5000, pointerOverflowBehaviour = PointerOverflowBehaviour.WRAP)
+        thread {
+            p.uid = dao.insert(p)
+            with(p) {
+                name = "New name"
+                source = "Modified source"
+                minVal = 5300
+                pointerOverflowBehaviour = PointerOverflowBehaviour.ERROR
+            }
+            dao.update(p)
+            assertEquals(p, dao.getProgram(p.uid))
+        }
+    }
 
 }
